78dc0a22f04d69d533b0c5679cdf4a469c82e79d
Gustavo Ribeiro <gustavo.mribeiroxg@yahoo.com>
2023-01-16 12:44:14 -0300

    adding animate on imageView

--- app/src/main/java/com/dev/myappcompany/animations/MainActivity.java
+++ app/src/main/java/com/dev/myappcompany/animations/MainActivity.java
@@ -5,12 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     public void fade(View view) {
 
         Log.i("Info", "Imageview tapped");
+
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+
+        imageView.animate().alpha(0).setDuration(2000);
 
     }
 
